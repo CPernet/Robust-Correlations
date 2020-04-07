7f805d3823ddb9ab2592286813971b4dffef5956
CPernet <wamcyril@gmail.com>
2020-04-07 12:26:02 +0100

    trailing and if flags fixed

--- Pearson.m
+++ Pearson.m
@@ -1,17 +1,17 @@
 function [r,t,pval,CI] = Pearson(X,Y,varargin)
 
 % Computes the Pearson correlation along with the alpha percent CI using
-% the Z-transform which gives a relativey good probability coverage under 
+% the Z-transform which gives a relativey good probability coverage under
 % normality assumptions. This returns in t(1) and pval(1) the same values
 % as the Matlab corrcoef function. When H0 is rejected (say pval<0.05),
 % it is reasonable to conclude that X and Y are dependent, but the nature of
 % that dependence is unclear. If fact, in case of heteroscedasticity, even for
 % rho = 0, H0 can be rejected because the t value relies on a wrong estimate
 % of the variance, i.e. the test statisitics t, tests if X and Y are independent
-% rather than testing if rho =0. Therefore, if pval is below the alpha level 
+% rather than testing if rho =0. Therefore, if pval is below the alpha level
 % (H0 rejected) it should be compared to a robust t-test version that
 % correct the covariance. Here, we return in t(2) and pval(2) such values
-% using the HC4 estimator of the standard error (Godfrey, 2006). If that 
+% using the HC4 estimator of the standard error (Godfrey, 2006). If that
 % second p-value is also below alpha, CI are retuned using the standard
 % z-transform. If pval is above alpha, but the robust estimate is below, the
 % discrepency is likely due to heteroscedasticity and no good CI can be
@@ -31,12 +31,12 @@ function [r,t,pval,CI] = Pearson(X,Y,varargin)
 %
 % OUTPUTS: r is the Pearson correlation
 %          t are the t-values using the standard equation and using the hc4 variance estimate
-%          pval are the p values corresponding to t-tests 
+%          pval are the p values corresponding to t-tests
 %          CI is 1-alpha confidence interval (only if all(pvals<alpha) or all(pvals>alpha))
 %
-% If 'figure' is on, the data scatter plot is shown with the least 
-% square fit of the data and the confidence intervals from the r value. As a 
-% sanity check, the boostrapped correlations are also shown reporting the 
+% If 'figure' is on, the data scatter plot is shown with the least
+% square fit of the data and the confidence intervals from the r value. As a
+% sanity check, the boostrapped correlations are also shown reporting the
 % median of bootstrapped value.
 %
 % References:
@@ -68,7 +68,7 @@ end
 for v=1:2:size(varargin,2)
     if strcmpi(varargin{v},'figure')
         figflag = varargin{v+1};
-    elseif strcmpi(varargin{v}(1:6),'hetero') 
+    elseif strcmpi(varargin{v}(1:6),'hetero')
         heteroscedasticity = varargin{v+1};
         if ~strcmpi(varargin{v},'heteroscedasticity')
             fprintf('taking argument in ''%s'' as heteroscedasticity \n',varargin{v})
@@ -84,112 +84,102 @@ end
 [n,p]  = size(X);
 alphav = alphav./ p;
 
-%% compute
+%% Compute
+r = sum(detrend(X,'constant').*detrend(Y,'constant')) ./ ...
+    (sum(detrend(X,'constant').^2).*sum(detrend(Y,'constant').^2)).^(1/2);
 
-r                        = sum(detrend(X,'constant').*detrend(Y,'constant')) ./ ...
-                           (sum(detrend(X,'constant').^2).*sum(detrend(Y,'constant').^2)).^(1/2);
-zr                       = 0.5 * log((1+r)./(1-r));
-zalpha                   = icdf('Normal',alphav/2,0,1);
-t                        = r.*sqrt((n-2)./(1-r.^2));
-pval                     = 2*tcdf(-abs(t),n-2);
-S                        = 1/sqrt(n-3); % assumed standard error
-if p==1
-    [t(2),pval(2),~,S_hc4] = get_hc4stats(r,zr,zscore(X,0,1),zscore(Y,0,1));
-else
-    [t(2,:),pval(2,:),~,S_hc4] = get_hc4stats(r,zr,zscore(X,0,1),zscore(Y,0,1));
-end
-
-for column = p:-1:1
-    if all(pval(:,column)<alphav) || all(pval(:,column)>alphav) || strcmpi(heteroscedasticity,'off')
-        CI(:,column) = tanh([zr(:,column)-abs(zalpha)*S ; zr(:,column)+abs(zalpha)*S]);
+if nargout > 1 || strcmpi(figflag ,'on')
+    zr                       = 0.5 * log((1+r)./(1-r));
+    zalpha                   = icdf('Normal',alphav/2,0,1);
+    t                        = r.*sqrt((n-2)./(1-r.^2));
+    pval                     = 2*tcdf(-abs(t),n-2);
+    S                        = 1/sqrt(n-3); % assumed standard error
+    if p==1
+        [t(2),pval(2),~,S_hc4] = get_hc4stats(r,zr,zscore(X,0,1),zscore(Y,0,1));
     else
-        CI(:,column) = [NaN NaN]';
+        [t(2,:),pval(2,:),~,S_hc4] = get_hc4stats(r,zr,zscore(X,0,1),zscore(Y,0,1));
     end
-end
-
-%% undocumented - do a percentile t for the hc4 CI (kinda work for n>80)
-if strcmpi(heteroscedasticity,'on')
     
-    for B=nboot:-1:1
-        % make a resampling table with enough unique pairs
-        go = 0; 
-        while go == 0
-            tmp = randi(n,n,1);
-            if length(unique(tmp))>=6
-                table(:,B) = tmp;
-                go = 1;
-            end
+    for column = p:-1:1
+        if all(pval(:,column)<alphav) || all(pval(:,column)>alphav) || strcmpi(heteroscedasticity,'off')
+            CI(:,column) = tanh([zr(:,column)-abs(zalpha)*S ; zr(:,column)+abs(zalpha)*S]);
+        else
+            CI(:,column) = [NaN NaN]';
         end
     end
     
-    % resample and compute
-    ibot = round(alphav*nboot/2)+1;
-    itop = nboot-ibot+2;
-    for column = p:-1:1
-        zX           = X(:,p);
-        zX           = zscore(zX(table),0,1); 
-        zY           = Y(:,p);
-        zY           = zscore(zY(table),0,1);
-        [~,~,B,S]    = get_hc4stats(r(column),zr(column),zX,zY); % all bootstraped betas and S
-        v            = sort((B-r(column))./sqrt(S));
-        CI(:,column) = [r(column)-v(itop)*sqrt(S_hc4(column)) r(column)-v(ibot)*sqrt(S_hc4(column))]';
+    %% undocumented - do a percentile t for the hc4 CI (kinda work for n>80)
+    if strcmpi(heteroscedasticity,'on')
+        
+        for B=nboot:-1:1
+            % make a resampling table with enough unique pairs
+            go = 0;
+            while go == 0
+                tmp = randi(n,n,1);
+                if length(unique(tmp))>=6
+                    boot_table(:,B) = tmp;
+                    go = 1;
+                end
+            end
+        end
+        
+        % resample and compute
+        ibot = round(alphav*nboot/2)+1;
+        itop = nboot-ibot+2;
+        for column = p:-1:1
+            zX           = X(:,p);
+            zX           = zscore(zX(boot_table),0,1);
+            zY           = Y(:,p);
+            zY           = zscore(zY(boot_table),0,1);
+            [~,~,B,S]    = get_hc4stats(r(column),zr(column),zX,zY); % all bootstraped betas and S
+            v            = sort((B-r(column))./sqrt(S));
+            CI(:,column) = [r(column)-v(itop)*sqrt(S_hc4(column)) r(column)-v(ibot)*sqrt(S_hc4(column))]';
+        end
     end
 end
 
 %% figure
 if strcmpi(figflag ,'on')
-    if ~exist('table','var')
-        table = randi(n,n,nboot);
+    if ~exist('boot_table','var')
+        boot_table = randi(n,n,nboot);
     end
     for B=nboot:-1:1
-        rb(B,:) = sum(detrend(X(table(:,B),:),'constant').*detrend(Y(table(:,B),:),'constant')) ./ ...
-            (sum(detrend(X(table(:,B),:),'constant').^2).*sum(detrend(Y(table(:,B),:),'constant').^2)).^(1/2);
+        rb(B,:) = sum(detrend(X(boot_table(:,B),:),'constant').*detrend(Y(boot_table(:,B),:),'constant')) ./ ...
+            (sum(detrend(X(boot_table(:,B),:),'constant').^2).*sum(detrend(Y(table(:,B),:),'constant').^2)).^(1/2);
     end
 end
-    
+
 if strcmpi(figflag ,'on')
     for f=1:length(r)
         figure('Name',sprintf('Pearson correlation X%g Y%g',f,f));
-        set(gcf,'Color','w');
-        
-        if nargout>3
-            subplot(1,2,1);
-            M = sprintf('r=%g \n %g%%CI [%.2f %.2f]',r(f),(1-alphav)*100,CI(1,f),CI(2,f));
-        else
-            M = sprintf('r=%g \n p=%g',r(f),pval(f));
-        end
-        
+        set(gcf,'Color','w'); subplot(1,2,1);
         scatter(X(:,f),Y(:,f),100,'filled'); grid on; box on;
         xlabel('X','FontSize',12); ylabel('Y','FontSize',12);
+        M = sprintf('r=%g \n %g%%CI [%.2f %.2f]',r(f),(1-alphav)*100,CI(1,f),CI(2,f));
         title(M,'FontSize',14); h=lsline; set(h,'Color','r','LineWidth',4);
         
-        if nargout>3 % if bootstrap done 
-            
-            subplot(1,2,2); k = round(1 + log2(nboot));
-            MV = histogram(rb(:,f),k); MV = max(MV.Values); grid on;
-            title(sprintf('Bootstrapped correlations \n median=%g',median(rb)),'FontSize',14);
-            xlabel('boot correlations','FontSize',12);ylabel('frequency','FontSize',12)
-            hold on; plot(repmat(CI(1,f),MV,1),1:MV,'r','LineWidth',4);
-            axis tight; colormap([.4 .4 1]); box on;
-            plot(median(rb),MV/2,'ko','LIneWidth',3)
-            
-            if all(~isnan(CI(:,f))) % plot CI
-                plot(repmat(CI(2,f),MV,1),1:MV,'r','LineWidth',4);
-                subplot(1,2,1); hold on
-                betas = pinv([X(:,f) ones(n,1)])*Y(:,f);
-                transform = betas(1)/r(f);
-                y1 = refline(CI(1,f)*transform,betas(2)); set(y1,'Color','r');
-                y2 = refline(CI(2,f)*transform,betas(2)); set(y2,'Color','r');
-                y1 = get(y1); y2 = get(y2);
-                xpoints=[y1.XData(1):y1.XData(2),y2.XData(2):-1:y2.XData(1)];
-                step1 = y1.YData(2)-y1.YData(1); step1 = step1 / (y1.XData(2)-y1.XData(1));
-                step2 = y2.YData(2)-y2.YData(1); step2 = step2 / (y2.XData(2)-y2.XData(1));
-                filled=[y1.YData(1):step1:y1.YData(2),y2.YData(2):-step2:y2.YData(1)];
-                hold on; fillhandle=fill(xpoints,filled,[1 0 0]);
-                set(fillhandle,'EdgeColor',[1 0 0],'FaceAlpha',0.2,'EdgeAlpha',0.8);%set edge color
-            end
-            
-
+        subplot(1,2,2); k = round(1 + log2(nboot));
+        MV = histogram(rb(:,f),k); MV = max(MV.Values); grid on;
+        title(sprintf('Bootstrapped correlations \n median=%g',median(rb)),'FontSize',14);
+        xlabel('boot correlations','FontSize',12);ylabel('frequency','FontSize',12)
+        hold on; plot(repmat(CI(1,f),MV,1),1:MV,'r','LineWidth',4);
+        axis tight; colormap([.4 .4 1]); box on;
+        plot(median(rb),MV/2,'ko','LIneWidth',3)
+        
+        if all(~isnan(CI(1:2,f))) % plot CI
+            plot(repmat(CI(2,f),MV,1),1:MV,'r','LineWidth',4);
+            subplot(1,2,1); hold on
+            betas = pinv([X(:,f) ones(n,1)])*Y(:,f);
+            transform = betas(1)/r(f);
+            y1 = refline(CI(1,f)*transform,betas(2)); set(y1,'Color','r');
+            y2 = refline(CI(2,f)*transform,betas(2)); set(y2,'Color','r');
+            y1 = get(y1); y2 = get(y2);
+            xpoints=[y1.XData(1):y1.XData(2),y2.XData(2):-1:y2.XData(1)];
+            step1 = y1.YData(2)-y1.YData(1); step1 = step1 / (y1.XData(2)-y1.XData(1));
+            step2 = y2.YData(2)-y2.YData(1); step2 = step2 / (y2.XData(2)-y2.XData(1));
+            filled=[y1.YData(1):step1:y1.YData(2),y2.YData(2):-step2:y2.YData(1)];
+            hold on; fillhandle=fill(xpoints,filled,[1 0 0]);
+            set(fillhandle,'EdgeColor',[1 0 0],'FaceAlpha',0.2,'EdgeAlpha',0.8);%set edge color
         end
     end
 end
--- Spearman.m
+++ Spearman.m
@@ -1,4 +1,4 @@
-function [r,t,pval,hboot,CI] = Spearman(X,Y,fig_flag,level)
+function [r,t,pval,CI] = Spearman(X,Y,varargin)
 
 % Computes the Spearman correlation with its bootstrap CI.
 %
@@ -30,181 +30,146 @@ function [r,t,pval,hboot,CI] = Spearman(X,Y,fig_flag,level)
 % ---------------------------------
 %  Copyright (C) Corr_toolbox 2012
 
-%% data check
-
-% if X a vector and Y a matrix, 
-% repmat X to perform multiple tests on Y (or the other around)
-if size(X,1) == 1 && size(X,2) > 1; X = X'; end
-if size(Y,1) == 1 && size(Y,2) > 1; Y = Y'; end
-
-if size(X,2) == 1 && size(Y,2) > 1
-    X = repmat(X,1,size(Y,2));
-elseif size(Y,2) == 1 && size(X,2) > 1
-    Y = repmat(Y,1,size(X,2));
-end
-
-if sum(size(X)~=size(Y)) ~= 0
-    error('X and Y must have the same size')
-end
-
-%% parameters
+%% input checks
 if nargin < 2
     error('two inputs requested');
-elseif nargin == 2
-    fig_flag = 1;
-    level = 5/100;
-elseif nargin == 3
-    level = 5/100;
+else
+    [X,Y] = check_corr_XY(X,Y);
 end
 
-[n p] = size(X);
+% defaults
+nboot              = 1000;
+alphav             = 5/100;
+heteroscedasticity = 'unspecified'; % undocumented, on/off forces to return a CI (never a NaN)
+if size(X,2) == 1 % and therefore size(Y,2) = 1 as well
+    figflag = 'on';
+else
+    figflag = 'off';
+end
 
-%% basic Spearman
+% check options
+for v=1:2:size(varargin,2)
+    if strcmpi(varargin{v},'figure')
+        figflag = varargin{v+1};
+    elseif strcmpi(varargin{v}(1:6),'hetero') 
+        heteroscedasticity = varargin{v+1};
+        if ~strcmpi(varargin{v},'heteroscedasticity')
+            fprintf('taking argument in ''%s'' as heteroscedasticity \n',varargin{v})
+        end
+    elseif strcmpi(varargin{v},'alpha')
+        alphav = varargin{v+1};
+        if ~isnumeric(alphav)
+            alphav = str2double(alphav);
+        end
+    end
+end
 
-% compute r (default)
+[n,p]  = size(X);
+alphav = alphav./ p;
+
+
+%% Compute 
 Xrank = tiedrank(X,0);
 Yrank = tiedrank(Y,0);
 r = sum(detrend(Xrank,'constant').*detrend(Yrank,'constant')) ./ ...
     (sum(detrend(Xrank,'constant').^2).*sum(detrend(Yrank,'constant').^2)).^(1/2);
-t = r.*(sqrt(n-2)) ./ sqrt((1-r.^2));
-pval = 2*tcdf(-abs(t),n-2);
-% The corr function in the stat toolbox uses
-% permutations for n<10 and some other fancy
-% things when n>10 and there are no ties among
-% ranks - we just do the standard way.
 
-if nargout > 3
-    nboot = 1000;
-    if p > 1
-        level = level / p;
-    end
-    low = round((level*nboot)/2);
-    if low == 0
-        error('adjusted CI cannot be computed, too many tests for the number of observations')
+% stats
+if nargout > 1 || strcmpi(figflag ,'on')
+    zr      = 0.5 * log((1+r)./(1-r));
+    zalpha  = icdf('Normal',alphav/2,0,1);
+    t       = r.*(sqrt(n-2)) ./ sqrt((1-r.^2));
+    pval    = 2*tcdf(-abs(t),n-2);
+    S       = 1/sqrt(n-3); % assumed standard error
+    if p==1
+        [t(2),pval(2),~,S_hc4]     = get_hc4stats(r,zr,zscore(X,0,1),zscore(Y,0,1));
     else
-        high = nboot - low;
+        [t(2,:),pval(2,:),~,S_hc4] = get_hc4stats(r,zr,zscore(X,0,1),zscore(Y,0,1));
     end
-
-    % bootstrap
-    table = randi(n,n,nboot);
-    for B=1:nboot
-        xrank = tiedrank(X(table(:,B),:),0);
-        yrank = tiedrank(Y(table(:,B),:),0);
-        rb(B,:) = sum(detrend(xrank,'constant').*detrend(yrank,'constant')) ./ ...
-            (sum(detrend(xrank,'constant').^2).*sum(detrend(yrank,'constant').^2)).^(1/2);
-        for c=1:size(X,2)
-            b = pinv([xrank(:,c) ones(n,1)])*yrank(:,c);
-            slope(B,c) = b(1); intercept(B,c) = b(2,:);
+    
+    for column = p:-1:1
+        if all(pval(:,column)<alphav) || all(pval(:,column)>alphav) || strcmpi(heteroscedasticity,'off')
+            CI(:,column) = tanh([zr(:,column)-abs(zalpha)*S ; zr(:,column)+abs(zalpha)*S]);
+        else
+            CI(:,column) = [NaN NaN]';
         end
     end
     
-    rb = sort(rb,1);
-    slope = sort(slope,1);
-    intercept = sort(intercept,1);
-    
-    % CI and h
-    adj_nboot = nboot - sum(isnan(rb));
-    adj_low = round((level*adj_nboot)/2);
-    adj_high = adj_nboot - adj_low;
-    
-    for c=1:size(X,2)
-        CI(:,c) = [rb(adj_low(c),c) ; rb(adj_high(c),c)];
-        hboot(c) = (rb(adj_low(c),c) > 0) + (rb(adj_high(c),c) < 0);
-        CIslope(:,c) = [slope(adj_low(c),c) ; slope(adj_high(c),c)];
-        CIintercept(:,c) = [intercept(adj_low(c),c) ; intercept(adj_high(c),c)];
+    %% undocumented - do a percentile t for the hc4 CI (kinda work for n>80)
+    if strcmpi(heteroscedasticity,'on')
+        
+        for B=nboot:-1:1
+            % make a resampling boot_table with enough unique pairs
+            go = 0;
+            while go == 0
+                tmp = randi(n,n,1);
+                if length(unique(tmp))>=6
+                    boot_table(:,B) = tmp;
+                    go = 1;
+                end
+            end
+        end
+        
+        % resample and compute
+        ibot = round(alphav*nboot/2)+1;
+        itop = nboot-ibot+2;
+        for column = p:-1:1
+            zX           = X(:,p);
+            zX           = zscore(tiedrank(zX(boot_table),0),0,1);
+            zY           = Y(:,p);
+            zY           = zscore(tiedrank(zY(boot_table),0),0,1);
+            [~,~,B,S]    = get_hc4stats(r(column),zr(column),zX,zY); % all bootstraped betas and S
+            v            = sort((B-r(column))./sqrt(S));
+            CI(:,column) = [r(column)-v(itop)*sqrt(S_hc4(column)) r(column)-v(ibot)*sqrt(S_hc4(column))]';
+        end
     end
 end
 
-%% plots
-if fig_flag ~= 0
-    answer = [];
-    if p > 1
-        answer = questdlg(['plots all ' num2str(p) ' correlations'],'Plotting option','yes','no','yes');
-    else
-        if fig_flag == 1
-            figure('Name','Spearman correlation');
-            set(gcf,'Color','w'); 
-        end
+%% figure
+if strcmpi(figflag ,'on')
+    if ~exist('boot_table','var')
+        boot_table = randi(n,n,nboot);
+    end
+    for B=nboot:-1:1
+        rX = tiedrank(X(boot_table(:,B),:),0); 
+        rY = tiedrank(Y(boot_table(:,B),:),0);
+        rb(B,:) = sum(detrend(rX,'constant').*detrend(rY,'constant')) ./ ...
+            (sum(detrend(rX,'constant').^2).*sum(detrend(rY,'constant').^2)).^(1/2);
+    end
+end
+
+if strcmpi(figflag ,'on')
+    for f=1:length(r)
+        figure('Name',sprintf('Spearman correlation X%g Y%g',f,f));
+        set(gcf,'Color','w'); subplot(1,2,1);
+        scatter(X(:,f),Y(:,f),100,'filled'); grid on; box on;
+        xlabel('X','FontSize',12); ylabel('Y','FontSize',12);
+        M = sprintf('r=%g \n %g%%CI [%.2f %.2f]',r(f),(1-alphav)*100,CI(1,f),CI(2,f));
+        title(M,'FontSize',14); h=lsline; set(h,'Color','r','LineWidth',4);
         
-        if nargout>3
-            subplot(1,2,1);
-            M = sprintf('Spearman corr r=%g \n %g%%CI [%g %g]',r,(1-level)*100,CI(1),CI(2));
-        else
-            M = sprintf('Spearman corr r=%g p=%g',r,pval);
-        end
+        subplot(1,2,2); k = round(1 + log2(nboot));
+        MV = histogram(rb(:,f),k); MV = max(MV.Values); grid on;
+        title(sprintf('Bootstrapped correlations \n median=%g',median(rb)),'FontSize',14);
+        xlabel('boot correlations','FontSize',12);ylabel('frequency','FontSize',12)
+        hold on; plot(repmat(CI(1,f),MV,1),1:MV,'r','LineWidth',4);
+        axis tight; colormap([.4 .4 1]); box on;
+        plot(median(rb),MV/2,'ko','LIneWidth',3)
         
-        scatter(Xrank,Yrank,100,'filled'); grid on
-        xlabel('X Rank','FontSize',14); ylabel('Y Rank','FontSize',14);
-        title(M,'FontSize',16); 
-        h=lsline; set(h,'Color','r','LineWidth',4);
-        box on;set(gca,'FontSize',14,'Layer','Top')
-        
-        if nargout >3
-            y1 = refline(CIslope(1),CIintercept(1)); set(y1,'Color','r');
-            y2 = refline(CIslope(2),CIintercept(2)); set(y2,'Color','r');
+        if all(~isnan(CI(1:2,f))) % plot CI
+            plot(repmat(CI(2,f),MV,1),1:MV,'r','LineWidth',4);
+            subplot(1,2,1); hold on
+            betas = pinv([X(:,f) ones(n,1)])*Y(:,f);
+            transform = betas(1)/r(f);
+            y1 = refline(CI(1,f)*transform,betas(2)); set(y1,'Color','r');
+            y2 = refline(CI(2,f)*transform,betas(2)); set(y2,'Color','r');
             y1 = get(y1); y2 = get(y2);
-            xpoints=[[y1.XData(1):y1.XData(2)],[y2.XData(2):-1:y2.XData(1)]];
+            xpoints=[y1.XData(1):y1.XData(2),y2.XData(2):-1:y2.XData(1)];
             step1 = y1.YData(2)-y1.YData(1); step1 = step1 / (y1.XData(2)-y1.XData(1));
             step2 = y2.YData(2)-y2.YData(1); step2 = step2 / (y2.XData(2)-y2.XData(1));
-            filled=[[y1.YData(1):step1:y1.YData(2)],[y2.YData(2):-step2:y2.YData(1)]];
+            filled=[y1.YData(1):step1:y1.YData(2),y2.YData(2):-step2:y2.YData(1)];
             hold on; fillhandle=fill(xpoints,filled,[1 0 0]);
             set(fillhandle,'EdgeColor',[1 0 0],'FaceAlpha',0.2,'EdgeAlpha',0.8);%set edge color
-            box on;set(gca,'FontSize',14)
-            
-            subplot(1,2,2); hold on
-            k = round(1 + log2(length(rb))); hist(rb,k); grid on;
-            title({'Bootstrapped correlations';['h=',num2str(hboot)]},'FontSize',16)
-            xlabel('boot correlations','FontSize',14);ylabel('frequency','FontSize',14)
-            plot(repmat(CI(1),max(hist(rb,k)),1),[1:max(hist(rb,k))],'r','LineWidth',4);
-            plot(repmat(CI(2),max(hist(rb,k)),1),[1:max(hist(rb,k))],'r','LineWidth',4);
-            axis tight; colormap([.4 .4 1])
-            box on;set(gca,'FontSize',14,'Layer','Top')
-        end
-    end
-    
-    if strcmp(answer,'yes')
-        for f = 1:p
-            figure('Color','w','Name',[num2str(f) ' Spearman correlation'])
-            
-            if nargout >3
-                subplot(1,2,1);
-                M = sprintf('Spearman corr r=%g \n %g%%CI [%g %g]',r(f),(1-level)*100,CI(1,f),CI(2,f));
-            else
-                M = sprintf('Spearman corr r=%g p=%g',r(f),pval(f));
-            end
-            
-            scatter(Xrank(:,f),Yrank(:,f),100,'filled'); grid on
-            xlabel('X Rank','FontSize',14); ylabel('Y Rank','FontSize',14);
-            title(M,'FontSize',16);
-            h=lsline; set(h,'Color','r','LineWidth',4);
-            
-            if nargout >3
-                y1 = refline(CIslope(1,f),CIintercept(1,f)); set(y1,'Color','r');
-                y2 = refline(CIslope(2,f),CIintercept(2,f)); set(y2,'Color','r');
-                y1 = get(y1); y2 = get(y2);
-                xpoints=[[y1.XData(1):y1.XData(2)],[y2.XData(2):-1:y2.XData(1)]];
-                step1 = y1.YData(2)-y1.YData(1); step1 = step1 / (y1.XData(2)-y1.XData(1));
-                step2 = y2.YData(2)-y2.YData(1); step2 = step2 / (y2.XData(2)-y2.XData(1));
-                filled=[[y1.YData(1):step1:y1.YData(2)],[y2.YData(2):-step2:y2.YData(1)]];
-                hold on; fillhandle=fill(xpoints,filled,[1 0 0]);
-                set(fillhandle,'EdgeColor',[1 0 0],'FaceAlpha',0.2,'EdgeAlpha',0.8);%set edge color
-                box on;set(gca,'FontSize',14)
-                
-                subplot(1,2,2); hold on
-                k = round(1 + log2(length(rb(:,f)))); hist(rb(:,f),k); grid on;
-                plot(repmat(CI(1,f),max(hist(rb(:,f),k)),1),[1:max(hist(rb(:,f),k))],'r','LineWidth',4);
-                plot(repmat(CI(2,f),max(hist(rb(:,f),k)),1),[1:max(hist(rb(:,f),k))],'r','LineWidth',4);
-                title({'Bootstrapped correlations';['h=',num2str(hboot(f))]},'FontSize',16)
-                xlabel('boot correlations','FontSize',14);ylabel('frequency','FontSize',14)
-                axis tight; colormap([.4 .4 1])
-                box on;set(gca,'FontSize',14,'Layer','Top')
-            end
         end
     end
 end
-
-
-
-
-
 
