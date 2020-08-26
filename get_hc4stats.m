cd89228b70964a1bc7515ad9b26ff85a5591b415
CPernet <wamcyril@gmail.com>
2020-08-26 14:13:59 +0100

    hc4 stats

--- get_hc4stats.m
+++ get_hc4stats.m
@@ -1,12 +1,11 @@
-function [t,pval,B,V] = get_hc4stats(r,zr,zX,zY)
+function [t,pval,B,V] = get_hc4stats(r,zX,zY)
 
-% sub-routine for compute the t,pval and CI using the HC4 estimator of the
-% variance 
+% sub-routine for compute the t,pval and CI 
+% using the HC4 estimator of the variance 
 
 [n,p]=size(zX);
 if size(r) == [1 1]
     r  = repmat(r,1,p);
-    zr = repmat(zr,1,p);
 end
 
 for column = p:-1:1
@@ -14,11 +13,12 @@ for column = p:-1:1
     Betas         = pinv(D)*zY(:,column);
     B(column)     = Betas(2);
     residuals     = zY(:,column) - D*Betas;
+    H             = inv(D'*D);
     for row=n:-1:1
-        h(row,:)  = D(row,:)*inv(D'*D)*D(row,:)';
+        h(row,:)  = D(row,:)*H*D(row,:)';
     end
     d             = min(4,h/mean(h)); %n*h / sum(h)
-    S             = inv(D'*D)*D'*diag((residuals.^2)./((1-h).^d))*D*inv(D'*D);
+    S             = H*D'*diag((residuals.^2)./((1-h).^d))*D*H;
     S             = diag(S); 
     V(column)     = S(2:end); % estimates of squared standard error of r
     t(column)     = r(column)/sqrt(V(column));
